708135caaf859f9dbfb30f864ee09bc1608aa6f3
Ayla16454646587 <Aylakhan740@gmail.com>
2025-06-19 18:56:17 +0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,8 @@
+ayla=28
+amirah=28
+father=27
+mother=13
+print("aylas birthday is",ayla)
+print("amirahs birthday is",amirah)
+print("my fathers birthday is",father)
+print("my mothers birthday is",mother)
