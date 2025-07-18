f70dcb1c965846c9d88a2d61126ae7de59eb6e8c
Ayla16454646587 <Aylakhan740@gmail.com>
2025-07-18 18:10:43 +0500

    Add files via upload

---
+++ precendence.py
@@ -0,0 +1,14 @@
+V=4
+W=5
+x=8
+y=2
+z=0
+z=(V+W)*x/y;
+print("value of (v+w)* x/y is",z)
+
+name="Alex"
+age=0
+if name == "Alex" or name == "john" and age >= 2:
+    print("Hello, welcome.")
+else:
+    print("goodbye")
