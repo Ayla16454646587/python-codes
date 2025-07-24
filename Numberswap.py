4d47d66cfb4451e21e29ce07020c0d44e4a8cf58
Ayla16454646587 <Aylakhan740@gmail.com>
2025-07-24 13:05:06 +0500

    Add files via upload

---
+++ Numberswap.py
@@ -0,0 +1,10 @@
+
+a=int(input("Enter first number (a):"))
+b=int(input("Enter second number (b):"))
+c=int(input("Enter third number (c):"))
+
+print(f"\nBefore swapping:a={a},b={b},c={c}")
+
+a,b,c=c,a,b
+
+print(f"After swapping: a={a},b={b},c={c}")
