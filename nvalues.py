3cb1d19e58b05826676e0a58b3dc0cd68ac32014
Ayla16454646587 <Aylakhan740@gmail.com>
2025-07-30 18:26:25 +0500

    Add files via upload

---
+++ nvalues.py
@@ -0,0 +1,5 @@
+base=float(input("Enter a base number:"))
+exponent=int(input("Enter the exponent (power):"))
+
+result=base**exponent
+print(f"{base}raised to the power of {exponent} is: {result}")
